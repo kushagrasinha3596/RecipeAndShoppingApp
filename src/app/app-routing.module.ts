0f15c5b2efe8cd47696647e1a8f0e14a5a7426d4
kushagrasinha3596 <kushagrasinha14.cse.msit@gmail.com>
2019-11-10 02:26:45 -0800

    Deleting unwanted imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ReciepeBookComponent } from './reciepe-book/reciepe-book.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ReciepeDetailsComponent } from './reciepe-book/reciepe-details/reciepe-details.component';
-import { ReciepeListComponent } from './reciepe-book/reciepe-list/reciepe-list.component';
-import { ReciepeItemComponent } from './reciepe-book/reciepe-list/reciepe-item/reciepe-item.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipehomepageComponent } from './reciepe-book/recipehomepage/recipehomepage.component';
-import { RecipeeditComponent } from './reciepe-book/recipeedit/recipeedit.component';
-import { RecipeResolver } from './reciepe-book/recipe-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/authguard';
 
 
 const routes: Routes = [
